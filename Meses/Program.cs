5899d15fde442d5db6d4533f24b68266e296b60b
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-11-02 21:35:22 -0700

    Agregar archivos de proyecto.

---
+++ Meses/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Meses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            string[] meses = new string[12];
+
+            meses[0] = "Enero";
+            meses[1] = "Febrero";
+            meses[2] = "Marzo";
+            meses[3] = "Abril";
+            meses[4] = "Mayo";
+            meses[5] = "Junio";
+            meses[6] = "Julio";
+            meses[7] = "Agosto";
+            meses[8] = "Septiembre";
+            meses[9] = "Octubre";
+            meses[10] = "Noviembre";
+            meses[11] = "Diciembre";
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out int result);
+
+            if (result > 0 && result <= 12)
+            {
+                Console.WriteLine(meses[result - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Dato incorrecto");
+            }
+
+        }
+    }
+}
